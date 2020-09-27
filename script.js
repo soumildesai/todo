022fc13198a07e70aa739cb0e20b1bee4eed110f
soumildesai <71241969+soumildesai@users.noreply.github.com>
2020-09-26 23:55:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -123,14 +123,14 @@ function getTodos(){
     });
 }
 function removeLocalTodos(todo) {
-        //Check---Hey do i already have things in here?
-        let todos;
-        if(localStorage.getItem("todos") === null) {
-            todos = [];
-        } else {
-            todos = JSON.parse(localStorage.getItem("todos"));
-        }
-        
+    //Check---Hey do i already have things in here?
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
     const todoIndex = todo.children[0].innerText;
     todos.splice(todos.indexOf(todoIndex), 1);
     localStorage.setItem("todos", JSON.stringify(todos));
